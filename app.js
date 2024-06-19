8919cbe4ae9673cc469f969b956a5a7b752e17b9
Debiddo <dzheng@terpmail.umd.edu>
2024-06-19 19:41:44 -0400

    changed the gameboard to have [x,y] coordinates and place a single random tetrominoes

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-const grid = document.querySelector('.grid');
+const gameBoard = document.querySelector('.game-board');
 const score = document.querySelector('.score');
 const start = document.querySelector('.start');
-const width = 10;
 
+// create a game board
+function createGameBoard(board, cols, rows) {
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.dataset.x = x;
+            cell.dataset.y = y;
+            board.appendChild(cell);
+        }
+    }
+}
+createGameBoard(gameBoard,10,20);
 
-
-// adding 200 tetrominoes into the grid
-for(let i=0;i<200;i++){
-    const newDiv = document.createElement('div');
-    grid.appendChild(newDiv);
+// get cell by coordinate
+function getCell(x, y) {
+    return gameBoard.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
 }
 
-const squares = Array.from(document.querySelectorAll('.grid div'));
-console.log(squares);
+const cells = Array.from(document.querySelectorAll('.cell'));
 
 // Tetrominoes
-const iTetromino = [[0, 1], [1, 1], [2, 1], [3, 1]];
-const jTetromino = [[0, 0], [1, 0], [2, 0], [2, 1]];
-const lTetromino = [[0, 0], [1, 0], [2, 0], [0, 1]];
-const oTetromino = [[0, 0], [1, 0], [0, 1], [1, 1]];
-const sTetromino = [[1, 0], [2, 0], [0, 1], [1, 1]];
-const tTetromino = [[0, 0], [1, 0], [2, 0], [1, 1]];
-const zTetromino = [[0, 0], [1, 0], [1, 1], [2, 1]];
+const iTetromino = [[0, 0],[0,1],[0,2],[0,3]];
+const jTetromino = [[0, 0],[0, 1],[0, 2],[-1, 2]];
+const lTetromino = [[0, 0],[0, 1],[0, 2],[1, 2]];
+const oTetromino = [[0, 0],[1, 0],[0, 1],[1, 1]];
+const sTetromino = [[-1, 1],[0, 1],[-2, 2],[-1, 2]];
+const tTetromino = [[-1, 1],[0, 1],[1, 1],[0, 2]];
+const zTetromino = [[-1, 1],[0, 1],[0, 2],[1, 2]];
 
+const tetrominoes = [iTetromino,jTetromino,lTetromino,oTetromino,sTetromino,tTetromino,zTetromino];
+
+let startPosition = [5,0];
+let currentTetromino = tetrominoes[6];
+
+//place tetromino
+function placeTetromino(piece){
+    piece.forEach(index =>{
+        // index = [x,y]
+        console.log({index})
+        console.log({startPosition})
+        getCell(index[0]+startPosition[0],index[1]+startPosition[1]).classList.add('tetromino');
+    })
+}
+
+// placeTetromino(tetrominoes[5]);
+
+// place a random piece
+placeTetromino(tetrominoes[Math.floor(Math.random()*tetrominoes.length)]);
 
 
